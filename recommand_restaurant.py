7bc3c964e1c0e60180a831b0660a91d84bab9666
gut0118 <32003945+gut0118@users.noreply.github.com>
2023-12-28 13:01:26 +0900

    주석 제거

--- recommand_restaurant.py
+++ recommand_restaurant.py
@@ -4,8 +4,6 @@ import numpy as np
 import random
 from datetime import datetime
 
-# https://zzsza.github.io/mlops/2021/02/07/python-streamlit-dashboard/
-
 st.title(f'M&S팀 {datetime.today().strftime("%y년 %m월 %d일")} 점심메뉴')
 
 button_style = """
@@ -33,13 +31,10 @@ if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
     st.write("")
     st.subheader("탐험지수")
-    # exploration = st.selectbox("새로운 곳에 대한 가중치", [1, 2, 3, 4, 5])
-    option_exploration = st.selectbox("새로운 곳에 대한 가중치", ["평균값", "평균값*1.5", "평균값*0.7", "최대값", "최대값*1.3"])
 
-    # type_distance = list(data["도보거리"].unique()) # 도보 3분, 5분, 10분, 20분
-    # type_recommender = list(data["추천인"].unique())
+    option_exploration = st.selectbox("새로운 곳에 대한 가중치", ["평균값", "평균값*1.5", "평균값*0.7", "최대값", "최대값*1.3"])
+    
     type_recommender = ["김태훈", "윤상혁", "이광현", "이주원", "최희수"] 
-    # type_fresh = list(data["미방문"].unique()) # 미방문 1, 방문 0
     type_fresh = ["N", "Y"] # 0: N,  1: Y
 
     st.write("")
@@ -71,7 +66,6 @@ if uploaded_file is not None:
     else:
         pass
 
-    # data['최종가중치'].fillna(data['가중치'].mean()*exploration, inplace=True)
     data['최종가중치'].fillna(exploration, inplace=True)
 
     # 음식종류 필터링
@@ -95,8 +89,6 @@ if uploaded_file is not None:
             probabilities = data['최종가중치'] / data['최종가중치'].sum()
             chosen_row = data.sample(1, weights=probabilities)
             st.write("")
-            # st.write("추천 식당 :", chosen_row.iloc[0]['식당명'])
             st.subheader(f":green[{chosen_row.iloc[0]['식당명']}]")
-            # st.write("Combining **bold and :green[colored text] is totally** fine! Just like with other markdown features.")
             url = chosen_row.iloc[0]['링크']
             st.markdown(f"[{chosen_row.iloc[0]['식당명']} 방문하기]({url})", unsafe_allow_html=True)
